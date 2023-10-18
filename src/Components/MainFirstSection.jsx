6669125a7578b941a7f474d707531b35ad46d9b6
00kang <kdb070970@gmail.com>
2023-10-19 03:29:44 +0900

    메인 페이지의 섹션1 업데이트, css 수정

--- src/Components/MainFirstSection.jsx
+++ src/Components/MainFirstSection.jsx
@@ -5,11 +5,11 @@ import { SlNote, SlNotebook, SlPaperClip } from "react-icons/sl";
 function MainFirstSection() {
   return (
     <Section>
-      <TextStyle fontSize="40px" fontWeight="700" margin="0 0 20px 0">
+      <TextStyle fontSize="36px" fontWeight="700" margin="0 0 20px 0">
         이력서와 자기소개서 작성을 위한 <br />
         Chat GPT 활용 WEB
       </TextStyle>
-      <TextStyle fontSize="18px" margin="0 0 40px 0 ">
+      <TextStyle fontSize="16px" margin="0 0 40px 0 ">
         다음 STEP을 따라 입사 지원 시에 필요한 서류를 완성해보세요.
       </TextStyle>
 
@@ -18,10 +18,10 @@ function MainFirstSection() {
           <IconWrap>
             <SlNote size={28} color="#404040" />
           </IconWrap>
-          <TextStyle fontSize="24px" fontWeight="500" margin="0 0 10px 0 ">
+          <TextStyle fontSize="20px" fontWeight="500" margin="0 0 10px 0 ">
             STEP 01
           </TextStyle>
-          <TextStyle fontSize="14px" fontWeight="300">
+          <TextStyle fontSize="12px" fontWeight="300">
             마스터 이력서를
             <br />
             작성해 보세요.
@@ -32,10 +32,10 @@ function MainFirstSection() {
           <IconWrap>
             <SlNotebook size={28} color="#404040" />
           </IconWrap>
-          <TextStyle fontSize="24px" fontWeight="500" margin="0 0 10px 0 ">
+          <TextStyle fontSize="20px" fontWeight="500" margin="0 0 10px 0 ">
             STEP 02
           </TextStyle>
-          <TextStyle fontSize="14px" fontWeight="300">
+          <TextStyle fontSize="12px" fontWeight="300">
             마스터 자기소개서를
             <br />
             작성해 보세요.
@@ -46,10 +46,10 @@ function MainFirstSection() {
           <IconWrap>
             <SlPaperClip size={28} color="#404040" />
           </IconWrap>
-          <TextStyle fontSize="24px" fontWeight="500" margin="0 0 10px 0">
+          <TextStyle fontSize="20px" fontWeight="500" margin="0 0 10px 0">
             STEP 03
           </TextStyle>
-          <TextStyle fontSize="14px" fontWeight="300">
+          <TextStyle fontSize="12px" fontWeight="300">
             마스터 자기소개서를 이용해
             <br />
             기업별 자기소개서를
--- src/Components/MainFirstSection.style.jsx
+++ src/Components/MainFirstSection.style.jsx
@@ -2,37 +2,39 @@ import styled from "styled-components";
 
 export const Section = styled.div`
   background-color: #fcf7ff;
-  height: 500px;
-  width: 100%;
   display: flex;
   flex-direction: column;
   justify-content: center;
+  margin: 0 auto;
+  padding: 40px 0;
 `;
 
 export const StepSection = styled.div`
+  width: 1240px;
+  margin: 20px auto;
   display: flex;
   justify-content: space-evenly;
   padding: 0px 100px;
-  margin: 20px;
 `;
 export const StepObj = styled.div`
   background-color: #e0d8ff;
-  width: 240px;
-  height: 200px;
+  width: 200px;
+  height: 180px;
   border-radius: 30px;
   padding: 20px;
   box-sizing: border-box;
+  cursor: pointer;
 `;
 
 export const IconWrap = styled.div`
   display: flex;
   justify-content: center;
-  margin: 15px 0;
+  margin: 10px 0;
 `;
 
 export const TextStyle = styled.p`
   text-align: center;
   font-size: ${(props) => props.fontSize || "16px"};
-  font-weight: ${(props) => props.fontWeight || "normal"};
+  font-weight: ${(props) => props.fontWeight || "400"};
   margin: ${(props) => props.margin || "0"};
 `;
