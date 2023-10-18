021b98b131e118861c661ff965b93efb8546d6eb
00kang <kdb070970@gmail.com>
2023-10-19 03:30:19 +0900

    메인 페이지의 섹션2 구현, 기본 틀 잡기

--- src/Components/MainSecondSection.jsx
+++ src/Components/MainSecondSection.jsx
@@ -1,26 +1,72 @@
 import React from "react";
-import * as F from "./MainSecondSection.style";
+import { Section, TextStyle, TabContainer, Tabs, Table, Th, Td } from "./MainSecondSection.style";
 
 function MainSecondSection() {
+  const data = [
+    {
+      Id: 1,
+      CurrentProcess: "서류 지원 완료",
+      CompanyName: "(주)우아한형제들",
+      Position: "배민푸드서비스실 배민푸드서비스개발팀 프론트엔드",
+      Deadline: "2023-10-01",
+      SubmissionDate: "2023-09-30",
+      ResumeMaterial: "pdf",
+      CoverLetterMaterial: "pdf",
+    },
+    {
+      Id: 2,
+      CurrentProcess: "서류 작성 중",
+      CompanyName: "(주)우아한형제들",
+      Position: "로봇서비스개발팀 프론트엔드",
+      Deadline: "2023-10-01",
+      SubmissionDate: "2023-09-30",
+      ResumeMaterial: "pdf",
+      CoverLetterMaterial: "pdf",
+    },
+  ];
+
   return (
     <div>
-      <F.Section2>
-        <h3>입사 지원 관리 현황</h3>
-        <table>
+      <Section>
+        <TextStyle fontSize="22px" fontWeight="600" margin="0 0 20px 0">
+          입사 지원 관리 현황
+        </TextStyle>
+
+        <TabContainer>
+          <Tabs>서류 작성 중</Tabs>
+          <Tabs>서류 작성 완료</Tabs>
+          <Tabs>서류 지원 완료</Tabs>
+          <Tabs>서류 불합격</Tabs>
+          <Tabs>서류 합격</Tabs>
+        </TabContainer>
+
+        <Table>
           <thead>
             <tr>
-              <th>현재 프로세스</th>
-              <th>회사명</th>
-              <th>직무</th>
-              <th>마감일</th>
-              <th>작성일</th>
-              <th>이력서 자료</th>
-              <th>자기소개서 자료</th>
+              <Th>현재 프로세스</Th>
+              <Th>회사명</Th>
+              <Th>직무</Th>
+              <Th>마감일</Th>
+              <Th>작성일</Th>
+              <Th>이력서 자료</Th>
+              <Th>자기소개서 자료</Th>
             </tr>
           </thead>
-          <tbody></tbody>
-        </table>
-      </F.Section2>
+          <tbody>
+            {data.map((item) => (
+              <tr key={item.Id}>
+                <Td>{item.CurrentProcess}</Td>
+                <Td>{item.CompanyName}</Td>
+                <Td>{item.Position}</Td>
+                <Td>{item.Deadline}</Td>
+                <Td>{item.SubmissionDate}</Td>
+                <Td>{item.ResumeMaterial}</Td>
+                <Td>{item.CoverLetterMaterial}</Td>
+              </tr>
+            ))}
+          </tbody>
+        </Table>
+      </Section>
     </div>
   );
 }
--- src/Components/MainSecondSection.style.jsx
+++ src/Components/MainSecondSection.style.jsx
@@ -1,5 +1,55 @@
 import styled from "styled-components";
 
-export const Section2 = styled.div`
-  background-color: #d1d1ff;
+export const TextStyle = styled.p`
+  text-align: center;
+  font-size: ${(props) => props.fontSize || "16px"};
+  font-weight: ${(props) => props.fontWeight || "400"};
+  margin: ${(props) => props.margin || "0"};
+`;
+
+export const Section = styled.div`
+  width: 1240px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  margin: 0 auto;
+  padding: 40px 0;
+`;
+
+export const TabContainer = styled.div`
+  display: flex;
+  /* border: 1px solid #ccc; */
+  /* border-radius: 5px; */
+  overflow: hidden;
+`;
+export const Tabs = styled.div`
+  flex: 1;
+  padding: 10px 0;
+  text-align: center;
+  cursor: pointer;
+
+  &:hover {
+    background-color: #f4ebff;
+    border-bottom: 4px solid #bcb3da;
+  }
+`;
+
+export const Table = styled.table`
+  width: 100%;
+  border-collapse: collapse;
+  margin: 0 auto;
+`;
+export const Th = styled.th`
+  height: 30px;
+  padding: 10px;
+  text-align: center;
+  border-bottom: 3px double #dedede;
+`;
+export const Td = styled.td`
+  height: 25px;
+  padding: 10px;
+  text-align: center;
+  border-bottom: 1px solid #dedede;
+  font-size: 12px;
+  font-weight: 300;
 `;
