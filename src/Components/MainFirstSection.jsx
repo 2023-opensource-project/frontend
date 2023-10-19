d7e7c6d284e74a90a72f874b410d68a9fbb3194d
00kang <kdb070970@gmail.com>
2023-10-20 04:03:12 +0900

    메인 페이지의 섹션1&2 업데이트, 컴포넌트 분리

--- src/Components/MainFirstSection.jsx
+++ src/Components/MainFirstSection.jsx
@@ -1,6 +1,6 @@
 import React from "react";
-import { Section, Inner, StepSection, StepObj, IconWrap, TextStyle } from "./MainFirstSection.style";
-import { SlNote, SlNotebook, SlPaperClip } from "react-icons/sl";
+import { Section, Inner, TextStyle } from "./MainFirstSection.style";
+import MainStepSection from "./MainStepSection";
 
 function MainFirstSection() {
   return (
@@ -14,51 +14,7 @@ function MainFirstSection() {
           다음 STEP을 따라 입사 지원 시에 필요한 서류를 완성해보세요.
         </TextStyle>
 
-        <StepSection>
-          <StepObj>
-            <IconWrap>
-              <SlNote size={28} color="#404040" />
-            </IconWrap>
-            <TextStyle fontSize="20px" fontWeight="500" margin="0 0 10px 0 ">
-              STEP 01
-            </TextStyle>
-            <TextStyle fontSize="12px" fontWeight="300">
-              마스터 이력서를
-              <br />
-              작성해 보세요.
-            </TextStyle>
-          </StepObj>
-
-          <StepObj>
-            <IconWrap>
-              <SlNotebook size={28} color="#404040" />
-            </IconWrap>
-            <TextStyle fontSize="20px" fontWeight="500" margin="0 0 10px 0 ">
-              STEP 02
-            </TextStyle>
-            <TextStyle fontSize="12px" fontWeight="300">
-              마스터 자기소개서를
-              <br />
-              작성해 보세요.
-            </TextStyle>
-          </StepObj>
-
-          <StepObj>
-            <IconWrap>
-              <SlPaperClip size={28} color="#404040" />
-            </IconWrap>
-            <TextStyle fontSize="20px" fontWeight="500" margin="0 0 10px 0">
-              STEP 03
-            </TextStyle>
-            <TextStyle fontSize="12px" fontWeight="300">
-              마스터 자기소개서를 이용해
-              <br />
-              기업별 자기소개서를
-              <br />
-              완성해 보세요.
-            </TextStyle>
-          </StepObj>
-        </StepSection>
+        <MainStepSection />
       </Inner>
     </Section>
   );
--- src/Components/MainSecondSection.jsx
+++ src/Components/MainSecondSection.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
-import { Section, Inner, TextStyle, TabContainer, Tab, Table, Th, Td } from "./MainSecondSection.style";
+import { Section, Inner, TextStyle, TabContainer, Tab } from "./MainSecondSection.style";
+import MainTabContent from "./MainTabContent";
 
 function MainSecondSection() {
   const data = [
@@ -102,32 +103,7 @@ function MainSecondSection() {
             </Tab>
           </TabContainer>
 
-          <Table>
-            <thead>
-              <tr>
-                <Th>현재 프로세스</Th>
-                <Th>회사명</Th>
-                <Th>직무</Th>
-                <Th>마감일</Th>
-                <Th>작성일</Th>
-                <Th>이력서 자료</Th>
-                <Th>자기소개서 자료</Th>
-              </tr>
-            </thead>
-            <tbody>
-              {filteredData.map((item) => (
-                <tr key={item.Id}>
-                  <Td>{item.CurrentProcess}</Td>
-                  <Td>{item.CompanyName}</Td>
-                  <Td>{item.Position}</Td>
-                  <Td>{item.Deadline}</Td>
-                  <Td>{item.SubmissionDate}</Td>
-                  <Td>{item.ResumeMaterial}</Td>
-                  <Td>{item.CoverLetterMaterial}</Td>
-                </tr>
-              ))}
-            </tbody>
-          </Table>
+          <MainTabContent filteredData={filteredData} />
         </Inner>
       </Section>
     </div>
