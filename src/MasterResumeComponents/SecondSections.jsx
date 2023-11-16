8cd3ee597eb06cf8c926e226fef52ddc4565d2f5
00kang <kdb070970@gmail.com>
2023-11-16 20:48:11 +0900

    feat(마스터 이력서): 경력 사항 파트에 폼 추가 버튼 구현

--- src/MasterResumeComponents/SecondSections.jsx
+++ src/MasterResumeComponents/SecondSections.jsx
@@ -98,24 +98,43 @@ const EducationInfo = () => {
 };
 
 const CareerInfo = () => {
+  const [careerCount, setCareerCount] = useState(1);
+
+  const handleAddCareer = (event) => {
+    event.preventDefault();
+    setCareerCount((prevCount) => prevCount + 1);
+  };
+
   return (
     <div style={{ margin: "20px 0 0 0" }}>
       <div style={{ borderBottom: "1px solid #ddd", marginBottom: "10px" }}>
         <TextStyle textAlign="left" fontSize="22px" fontWeight="600" margin="10px 0">
           경력 사항
+          <Button
+            type="button"
+            onClick={handleAddCareer}
+            backgroundColor="#F4EBFF"
+            width="30px"
+            height="30px"
+            margin="0 20px"
+          >
+            +
+          </Button>
         </TextStyle>
       </div>
-      <div style={{ backgroundColor: "#FAFAFA", padding: "10px" }}>
-        <LineWrapper>
-          <FormItem label="구분" type="dropdown" name="career" options={["아르바이트", "인턴쉽"]} />
-          <FormItem label="회사명" name="companyName" />
-          <FormItem label="직무" name="position" />
-        </LineWrapper>
-        <LineWrapper>
-          <FormItem label="재직 시작일" type="date" name="startWorkDate" />
-          <FormItem label="재직 종료일" type="date" name="endWorkDate" />
-        </LineWrapper>
-      </div>
+      {Array.from({ length: careerCount }, (_, index) => (
+        <ItemWrapper key={index}>
+          <LineWrapper>
+            <FormItem label="구분" type="dropdown" name="career" options={["아르바이트", "인턴쉽"]} />
+            <FormItem label="회사명" name="companyName" />
+            <FormItem label="직무" name="position" />
+          </LineWrapper>
+          <LineWrapper>
+            <FormItem label="재직 시작일" type="date" name="startWorkDate" />
+            <FormItem label="재직 종료일" type="date" name="endWorkDate" />
+          </LineWrapper>
+        </ItemWrapper>
+      ))}
     </div>
   );
 };
