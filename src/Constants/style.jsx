85474a568bd6be886013d886b03de92dfc067a29
00kang <kdb070970@gmail.com>
2023-11-16 17:18:54 +0900

    feat: 공통적으로 사용되는 스타일 속성 파일 생성

--- src/Constants/style.jsx
+++ src/Constants/style.jsx
@@ -8,8 +8,26 @@ export const Inner = styled.div`
 `;
 
 export const TextStyle = styled.p`
-  text-align: center;
+  text-align: ${(props) => props.textAlign || "center"};
   font-size: ${(props) => props.fontSize || "16px"};
   font-weight: ${(props) => props.fontWeight || "400"};
   margin: ${(props) => props.margin || "0"};
+`;
+
+export const Button = styled.button`
+  background-color: ${(props) => props.backgroundColor || "#E0D8FF"};
+  color: ${(props) => props.color || "#404040"};
+  width: ${(props) => props.width || "100px"};
+  height: ${(props) => props.height || "30px"};
+  font-size: ${(props) => props.fontSize || "14px"};
+  font-weight: ${(props) => props.fontWeight || "500"};
+  margin: ${(props) => props.margin || "0"};
+  text-align: center;
+  border-radius: 5px;
+  border: none;
+
+  &:hover {
+    border: 2px solid #9c9c9c;
+    cursor: pointer;
+  }
 `;
