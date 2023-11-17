a0d6a5873506dde56e23892abc9a7824952c9dcd
00kang <kdb070970@gmail.com>
2023-11-17 21:57:04 +0900

    feat(마스터 자기소개서): 타이틀 출력 구현

---
+++ src/MasterCoverLetterComponents/TitleContainer.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Title } from "../Constants/style";
+
+function TitleContainer() {
+  return (
+    <div style={{ borderBottom: "1px dashed #DEDEDE" }}>
+      <Title>마스터 자기소개서</Title>
+    </div>
+  );
+}
+
+export default TitleContainer;
